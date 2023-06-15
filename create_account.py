ec144bf75a7166ac31cca196b0514f666b12024a
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2023-06-15 17:42:38 -0600

    Not wishlisting item

--- create_account.py
+++ create_account.py
@@ -16,7 +16,7 @@ elem = browser.find_element(By.NAME, "lastname")
 elem.send_keys('Palotes')
 
 elem = browser.find_element(By.NAME, "email")
-elem.send_keys('perico3@yahoo.com')
+elem.send_keys('perico15@yahoo.com')
 
 elem = browser.find_element(By.NAME, "password")
 elem.send_keys('asdf1234%')
@@ -27,4 +27,34 @@ elem.send_keys('asdf1234%')
 elem = browser.find_element(By.XPATH, "//button[@title='Create an Account']")
 elem.click()
 
-browser.quit()
+#Aqui hacer la prueba
+# elem = browser.find_element(By.XPATH, "//html/body/div[1]/div/div/div[2]/nav/ul/li[3]/a/span[2]")
+# elem.click()
+
+elem = browser.find_element(By.LINK_TEXT, "Men")
+elem.click()
+
+elem = browser.find_element(By.LINK_TEXT, "Tops")
+elem.click()
+
+# Find the list of <li> elements
+li_list = browser.find_element(By.XPATH, "//a[@class='action towishlist']")
+
+first_li = li_list[0]
+first_li.click()
+
+# elem = browser.find_element(By.CLASS_NAME, "item product product-item")
+# elem.click()
+
+#elem = browser.find_element(By.LINK_TEXT, "Remove Item")
+#elem.click()
+
+
+
+# elem = browser.find_element(By.XPATH, "//html/body/div[1]/main/div[4]/div[2]/div[1]/div[2]/dl/dd/ol/li[1]/a")
+# elem.click()
+
+#elem = browser.find_element(By.XPATH, "/html/body/div[1]/main/div[3]/div[1]/div[3]/ol/li[1]/div/div/div[3]/div/div[2]/a[1]")
+#elem.click()
+
+#browser.quit()
